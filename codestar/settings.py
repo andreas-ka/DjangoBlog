56c7c6ab8497909c3a7f6e5ef0e568223e3c08e8
Andreas Karlsson <andreas.krln@hotmail.com>
2023-05-25 08:51:56 +0000

    Added likes and comments

--- codestar/settings.py
+++ codestar/settings.py
@@ -33,6 +33,9 @@ DEBUG = True
 ALLOWED_HOSTS = ['django-blog-codestar-ak.herokuapp.com', 'localhost']
 
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -41,12 +44,25 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    'crispy_forms',
     'blog',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
 MIDDLEWARE = [
